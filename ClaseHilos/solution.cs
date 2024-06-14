0a2fc1d56e80f8ac1b47397426c40f146b3d0d79
Leonel935 <Leonel.galende@uap.edu.ar>
2024-06-14 01:31:35 -0300

    Tp 11 Leonel Galende

--- ClaseHilos/solution.cs
+++ ClaseHilos/solution.cs
@@ -1,12 +1,16 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace ClaseHilos
 {
    internal class Producto
    {
       public string Nombre { get; set; }
-      public decimal PrecioUnitarioDolares { get; set; }
+      public double PrecioUnitarioDolares { get; set; }
       public int CantidadEnStock { get; set; }
 
-      public Producto(string nombre, decimal precioUnitario, int cantidadEnStock)
+      public Producto(string nombre, double precioUnitario, int cantidadEnStock)
       {
          Nombre = nombre;
          PrecioUnitarioDolares = precioUnitario;
@@ -27,22 +31,105 @@ namespace ClaseHilos
 
       static int precio_dolar = 500;
 
-      static void Tarea1()
+       static Barrier barrera = new Barrier(4, (b) =>
+        {
+            Console.WriteLine($"Post-Phase action: {b.CurrentPhaseNumber}");
+        });
+        public static Barrier barrera_
+        {
+            get { return barrera; }
+        }
+
+        static void Tarea1()
       {
-         throw new NotImplementedException();
-      }
-      static void Tarea2()
-      {
-         throw new NotImplementedException();
-      }
+         static void HolaUAP()
+            {
+                Console.WriteLine($"Proceso {Thread.CurrentThread.Name}");
+                Thread.Sleep(1500);
+                for (int i = 0; i < productos.Count; i++)
+                {
+                    productos[i].CantidadEnStock += 10;
+                    Console.WriteLine("Se actualizo la cantidad en stock de " + productos[i].Nombre + " a:" + productos[i].CantidadEnStock);
+                }
+                Console.WriteLine("----------------------------------------");
+                barrera.SignalAndWait();
+                Console.WriteLine("Hola desde " + Thread.CurrentThread.Name);
+
+            }
+            Thread task = new Thread(HolaUAP);
+            task.Name = "Hilo 1";
+            task.Start();
+        }
+        static void Tarea2()
+        {
+
+            static void HolaUAP()
+            {
+                Console.WriteLine($"Proceso {Thread.CurrentThread.Name}");
+                Thread.Sleep(2000);
+                //actualizoa el precio del dolar, día 7/6/24
+                precio_dolar = 1250;
+                Console.WriteLine("Se actualiza el precio del dolar");
+                Console.WriteLine("----------------------------------------");
+                barrera.SignalAndWait();
+                Console.WriteLine("Hola desde " + Thread.CurrentThread.Name);
+            }
+            Thread task = new Thread(HolaUAP);
+            task.Name = "Hilo 2";
+            task.Start();
+        }
       static void Tarea3()
       {
-         throw new NotImplementedException();
-      }
+         static void HolaUAP()
+            {
+                Console.WriteLine($"Proceso {Thread.CurrentThread.Name}");
+                Thread.Sleep(4000);
+                double contador = 0;
+                for (int i = 0; i < productos.Count; i++)
+                {
+                    Console.Write("-- >Producto: " + productos[i].Nombre + " - cantidad en stock:" + productos[i].CantidadEnStock);
+                    contador = contador + (productos[i].PrecioUnitarioDolares * productos[i].CantidadEnStock);
+                    Console.WriteLine($" - precio en dolar del producto: {productos[i].PrecioUnitarioDolares}");
 
-      internal static void Excecute()
-      {
-         throw new NotImplementedException();
+                }
+                Console.WriteLine($" Precio total del inventario en pesos: {contador * precio_dolar}");
+                Console.WriteLine("----------------------------------------");
+                barrera.SignalAndWait();
+                Console.WriteLine("Hola desde " + Thread.CurrentThread.Name);
+            }
+
+            Thread task = new Thread(HolaUAP);
+            task.Name = "Hilo 3";
+            task.Start();
       }
+ static void Tarea4()
+        {
+
+            static void HolaUAP()
+            {
+                Console.WriteLine($"Proceso {Thread.CurrentThread.Name}");
+                Thread.Sleep(3000);
+                for (int i = 0; i < productos.Count; i++)
+                {
+                    productos[i].PrecioUnitarioDolares *= 1.1;                    
+                }
+                Console.WriteLine("Se actualizo por inflación");
+                Console.WriteLine("----------------------------------------");
+                barrera.SignalAndWait();
+                Console.WriteLine("Hola desde " + Thread.CurrentThread.Name);
+      }
+            Thread task = new Thread(HolaUAP);
+            task.Name = "Hilo 4";
+            task.Start();
+        }
+        internal static void Excecute()
+      {
+         Tarea3();
+
+            Tarea2();
+
+            Tarea4();
+            Tarea1();
+        }
    }
 }
